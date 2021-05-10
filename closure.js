1b6ccb93a118d838b0ec09e54d121049e2b7f86f
KrishnaChittodai21 <krishnachittodia.10@gmail.com>
2021-05-10 16:01:40 +0530

    closures added

--- closure.js
+++ closure.js
@@ -4,7 +4,22 @@ function doThing(input1) {
   };
 }
 
-var callIt = doThing('Krishna');
-callIt('Chittodia');
+var callIt = doThing("Krishna");
+callIt("Chittodia");
 
 // inner function will have the reference of outer function memory even after it was removed from the execution stack
+
+function doThing2() {
+  var arr = [];
+  for (var i = 0; i < 3; i++) {
+    arr.push(function () {
+      console.log(i);
+    });
+  }
+  return arr;
+}
+
+var callIt2 = doThing2();
+callIt2[0]();
+callIt2[1]();
+callIt2[2]();
